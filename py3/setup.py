2838560b389db4ea96bdec3de70c80884b4daa1c
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-02-16 15:56:29 -0800

    0.23 un-unisos.

--- py3/setup.py
+++ py3/setup.py
@@ -43,7 +43,7 @@ def longDescription():
 # b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
 ####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
 
-# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.22 -- forSysVersion=0.21 -- constant=NA
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.23 -- forLocalVersion=0.21 -- constant=NA
 def pkgVersion():
         return '0.21'
 
@@ -53,8 +53,6 @@ def pkgVersion():
 ####+BEGIN: b:py3:pypi:setup/requires :extras ()
 
 requires = [
-"blee",
-"bisos.csPlayer",
 "bisos",
 "bisos.b",
 "bisos.common",
